0a6f44925fe7fa62318a3101b498cd380fcf4082
Edgar Carrillo <ecarrillo046@gmail.com>
2022-03-25 09:58:24 -0700

    make a post request that only updates default values

--- 2-checkout/server/models/users.js
+++ 2-checkout/server/models/users.js
@@ -31,10 +31,10 @@ module.exports = {
         [user.session_id]);
     })
     .then(() => {
-      const query = 'INSERT INTO users VALUES (null, ?, ?, ?, ?, 0, ?, ?);';
+      const query = 'INSERT INTO users VALUES (null, ?, null, null, null, 0, ?, ?);';
       db.query(
         query,
-        [user.session_id, user.name, user.email, user.password, user.session_id, user.session_id]);
+        [user.session_id, user.session_id, user.session_id]);
     })
     .then((response) => {
       db.commitAsync((response) => {
